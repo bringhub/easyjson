fa4d4cd14de09aaadc079ae7efb95836c77eae7f
Jessica Potts <jessica@bringhub.com>
2017-11-01 11:53:12 -0400

    NestedEasyMarshaler

--- tests/nested_easy.go
+++ tests/nested_easy.go
@@ -18,7 +18,7 @@ type NestedEasyMarshaler struct {
 
 var _ easyjson.Marshaler = &NestedEasyMarshaler{}
 
-func (i *NestedEasyMarshaler) MarshalEasyJSON(w *jwriter.Writer) {
+func (i *NestedEasyMarshaler) MarshalEasyJSON(w *jwriter.Writer, usingTagName string) {
 	// We use this method only to indicate that easyjson.Marshaler
 	// interface was really used while encoding.
 	i.EasilyMarshaled = true
